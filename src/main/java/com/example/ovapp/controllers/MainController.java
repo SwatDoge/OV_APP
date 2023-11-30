e4c7a21b977b3abfd13ca9f6b447a39e3ee99b50
maurits444 <maurits.vanbreemen@student.hu.nl>
2023-11-30 11:23:40 +0100

    Start- en eindlocatie mogelijk om steden te zoeken door middel van typen

--- src/main/java/com/example/ovapp/controllers/MainController.java
+++ src/main/java/com/example/ovapp/controllers/MainController.java
@@ -2,13 +2,16 @@ package com.example.ovapp.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class MainController {
+
     @FXML
-    private ChoiceBox<String> startCityChoiceBox;
+    private TextField startCityTextField;
 
     @FXML
     private ChoiceBox<String> startDepartArriveChoiceBox;
@@ -17,7 +20,7 @@ public class MainController {
     private DatePicker startDatePicker;
 
     @FXML
-    private ChoiceBox<String> endCityChoiceBox;
+    private TextField endCityTextField;
 
     @FXML
     private RadioButton busRadioButton;
@@ -28,26 +31,61 @@ public class MainController {
     @FXML
     private Button planReisButton;
 
+    private ObservableList<String> allCities = FXCollections.observableArrayList("Amersfoort", "Amsterdam", "Utrecht", "Ede", "Alkmaar", "Den Haag", "Groningen", "Deventer", "Nijkerk", "Apeldoorn", "Enschede");
+
     @FXML
     private void initialize() {
+        ObservableList<String> departArriveOptions = FXCollections.observableArrayList("Vertrek", "Aankomst");
+        startDepartArriveChoiceBox.setItems(departArriveOptions);
 
-        ObservableList<String> startCities = FXCollections.observableArrayList("Amersfoort", "Amsterdam", "Ede", "Alkmaar", "Den Haag");
-        ObservableList<String> endCities = FXCollections.observableArrayList("Groningen", "Deventer", "Nijkerk", "Apeldoorn", "Enschede");
+        // Configure city suggestions for start and end fields
+        configureCitySuggestions(startCityTextField);
+        configureCitySuggestions(endCityTextField);
+    }
 
-        startCityChoiceBox.setItems(startCities);
-        endCityChoiceBox.setItems(endCities);
-        startDepartArriveChoiceBox.setItems(FXCollections.observableArrayList("Vertrek", "Aankomst"));
+    private void configureCitySuggestions(TextField textField) {
+        FilteredList<String> filteredCities = new FilteredList<>(allCities, s -> true);
+
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredCities.setPredicate(city -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                String lowerCaseFilter = newValue.toLowerCase();
+                return city.toLowerCase().contains(lowerCaseFilter);
+            });
+        });
+
+        SortedList<String> sortedCities = new SortedList<>(filteredCities);
+
+        // Use a custom Popup for suggestions
+        CustomSuggestionPopup suggestionPopup = new CustomSuggestionPopup(sortedCities);
+
+        // Show suggestions when the user clicks the text field
+        textField.setOnMouseClicked(event -> suggestionPopup.show(textField));
+
+        // Hide suggestions when the user clicks outside the text field
+        textField.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (!newVal) {
+                suggestionPopup.hide();
+            }
+        });
+
+        // Set the selected suggestion in the text field when clicked
+        suggestionPopup.setOnSuggestionSelected(selectedItem -> {
+            textField.setText(String.valueOf(selectedItem));
+            suggestionPopup.hide();
+        });
     }
 
     @FXML
     private void onPlanReisButtonClick(ActionEvent event) {
-
-        String startCity = startCityChoiceBox.getValue();
+        String startCity = startCityTextField.getText();
         String startDepartArrive = startDepartArriveChoiceBox.getValue();
-        String endCity = endCityChoiceBox.getValue();
+        String endCity = endCityTextField.getText();
         String transport = busRadioButton.isSelected() ? "Bus" : "Trein";
         String date = startDatePicker.getValue() != null ? startDatePicker.getValue().toString() : "Geen datum geselecteerd";
-
 
         System.out.println("Start Stad: " + startCity);
         System.out.println("Vertrek/Aankomst: " + startDepartArrive);
