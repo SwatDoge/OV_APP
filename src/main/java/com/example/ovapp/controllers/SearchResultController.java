fdf449d63f5e656c6a99db9de6281b288c779477
Samvandop <samvandop@gmail.com>
2023-12-28 13:18:17 +0100

    added small function

--- src/main/java/com/example/ovapp/controllers/SearchResultController.java
+++ src/main/java/com/example/ovapp/controllers/SearchResultController.java
@@ -1,14 +1,15 @@
 package com.example.ovapp.controllers;
 
+import com.example.ovapp.TimeUtils;
 import com.example.ovapp.models.nsapi.NSApiRoot;
 import com.example.ovapp.models.nsapi.Trip;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import com.example.ovapp.TimeUtils;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -104,6 +105,9 @@ public class SearchResultController implements Initializable{
     @FXML
     //</editor-fold>
 
+    private NSApiRoot apiResult;
+
+
     //Pak de namen van de resultaten en zet ze in de labels.
     public void updateResultsDisplay(NSApiRoot nsApiRoot) {
         Trip[] trips = nsApiRoot.trips;
@@ -179,14 +183,54 @@ public class SearchResultController implements Initializable{
 
             arrival_route6.setText(formattedArrivalTime);
         }
+    }
+
+    public void setApiResult(NSApiRoot apiResult) {
+        this.apiResult = apiResult;
+        System.out.println("apiResult set: " + apiResult);
+
+    }
+
+    public NSApiRoot getApiResult() {
+        return apiResult;
+    }
+
+    private void updateDetails(int routeNumber) {
+        System.out.println("Updating details for route " + routeNumber + "...");
+        if (apiResult == null || apiResult.trips == null) {
+            System.out.println("NSApiRoot or trips is null.");
+            return;
+        }
+
+        Trip[] trips = apiResult.trips;
+        System.out.println("Number of trips: " + trips.length);
 
 
+        if (trips.length >= routeNumber) {
+            Trip selectedTrip = trips[routeNumber - 1];
+            System.out.println("Selected trip: " + selectedTrip);
+
+            Platform.runLater(() -> {
+                transfer_details.setText(String.format("%dx", selectedTrip.transfers));
+                during_details.setText(selectedTrip.getFormattedDuration());
+                departure_details.setText(selectedTrip.legs.get(0).origin.getFormattedTime());
+
+                String formattedDepartureTime = selectedTrip.legs.get(0).origin.getFormattedTime();
+                String formattedDuration = selectedTrip.getFormattedDuration();
+                String formattedArrivalTime = TimeUtils.calculateArrivalTime(formattedDepartureTime, formattedDuration);
+
+                arrival_details.setText(formattedArrivalTime);
+            });
+        }
     }
 
     public void handleRoute1ButtonClick(ActionEvent actionEvent) {
+        updateDetails(1);
     }
 
     public void handleRoute2ButtonClick(ActionEvent actionEvent) {
+        updateDetails(2);
+
     }
 
     public void handleRoute3ButtonClick(ActionEvent actionEvent) {
