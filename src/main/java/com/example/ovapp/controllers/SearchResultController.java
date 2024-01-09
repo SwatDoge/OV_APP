c427be91fa5727fad1bb93b5a014f0c0316d27b8
Samvandop <samvandop@gmail.com>
2024-01-09 14:51:43 +0100

    BOOM! de details van de route (rechterkant) werkt nu ook als je op een route drukt.

--- src/main/java/com/example/ovapp/controllers/SearchResultController.java
+++ src/main/java/com/example/ovapp/controllers/SearchResultController.java
@@ -19,9 +19,6 @@ import java.util.ResourceBundle;
 public class SearchResultController implements Initializable{
     @FXML
     private ScrollPane scrollPaneMain;
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-    }
 
     //Dit zijn alle labels die ik nu aanpas
     //<editor-fold desc="Elements">
@@ -107,10 +104,12 @@ public class SearchResultController implements Initializable{
     @FXML
     //</editor-fold>
 
-    private NSApiRoot apiResult;
+    private NSApiRoot currentApiResult;
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
 
-    //Pak de namen van de resultaten en zet ze in de labels.
     public void updateResultsDisplay(NSApiRoot nsApiRoot) {
         Trip[] trips = nsApiRoot.trips;
 
@@ -185,28 +184,29 @@ public class SearchResultController implements Initializable{
 
             arrival_route6.setText(formattedArrivalTime);
         }
+
+        currentApiResult = nsApiRoot;
     }
 
     public void setApiResult(NSApiRoot apiResult) {
-        this.apiResult = apiResult;
+        this.currentApiResult = apiResult;
         System.out.println("apiResult set: " + apiResult);
-
     }
 
     public NSApiRoot getApiResult() {
-        return apiResult;
+        return currentApiResult;
     }
 
     private void updateDetails(int routeNumber) {
         System.out.println("Updating details for route " + routeNumber + "...");
-        if (apiResult == null || apiResult.trips == null) {
+
+        if (currentApiResult == null || currentApiResult.trips == null) {
             System.out.println("NSApiRoot or trips is null.");
             return;
         }
 
-        Trip[] trips = apiResult.trips;
+        Trip[] trips = currentApiResult.trips;
         System.out.println("Number of trips: " + trips.length);
-
 
         if (trips.length >= routeNumber) {
             Trip selectedTrip = trips[routeNumber - 1];
@@ -232,23 +232,25 @@ public class SearchResultController implements Initializable{
 
     public void handleRoute2ButtonClick(ActionEvent actionEvent) {
         updateDetails(2);
-
     }
 
     public void handleRoute3ButtonClick(ActionEvent actionEvent) {
+        updateDetails(3);
     }
 
     public void handleRoute4ButtonClick(ActionEvent actionEvent) {
+        updateDetails(4);
     }
 
     public void handleRoute5ButtonClick(ActionEvent actionEvent) {
+        updateDetails(5);
     }
 
     public void handleRoute6ButtonClick(ActionEvent actionEvent) {
-
+        updateDetails(6);
     }
 
-    public void onBackButtonPressed() {
+public void onBackButtonPressed() {
         Page.navigateTo(EPage.HOME);
     }
 }
